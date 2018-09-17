d108d855276581a62cc8fddd46399469beb6d839
darshan218 <dgohel71@gmail.com>
2018-09-17 14:07:06 +0530

    solved

--- hello.py
+++ hello.py
@@ -3,8 +3,12 @@ print('Hello, world!')
 print('...!!!...')
 print('...!!!...')
 print('Hello, world!')
+<<<<<<< HEAD
 print('Hello, world!')
 print('Hello, world!')
 print('Hello, world!')
 
 
+=======
+print('33Hello, world!')
+>>>>>>> 783a88c... new cp
