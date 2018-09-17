bef31c8c208a4df6955bb83cf6fdf2b958d89c45
darshan218 <dgohel71@gmail.com>
2018-09-17 14:07:06 +0530

    aa

--- hello.py
+++ hello.py
@@ -3,9 +3,13 @@ print('Hello, world!')
 print('...!!!...')
 print('...!!!...')
 print('Hello, world!')
+<<<<<<< HEAD
 print('Hello, world!')
 print('Hello, world!')
 print('Hello, world!')
 
 
 
+=======
+print('33Hello, world!')
+>>>>>>> 783a88c... new cp
