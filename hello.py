edbf84e0bc31fc638660c5379ba8931cc8e6e8d4
darshan218 <dgohel71@gmail.com>
2018-09-17 14:07:06 +0530

    cc

--- hello.py
+++ hello.py
@@ -3,12 +3,9 @@ print('Hello, world!')
 print('...!!!...')
 print('...!!!...')
 print('Hello, world!')
-<<<<<<< HEAD
 print('Hello, world!')
 print('Hello, world!')
 print('Hello, world!')
 
 
-=======
-print('33Hello, world!')
->>>>>>> 783a88c... new cp
+
