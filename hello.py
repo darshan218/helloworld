a57cb793b80363a154bec40dafb71464e6f37a3d
darshan218 <dgohel71@gmail.com>
2018-09-17 14:07:06 +0530

    cp1

--- hello.py
+++ hello.py
@@ -4,12 +4,16 @@ print('...!!!...')
 print('...!!!...')
 print('Hello, world!')
 <<<<<<< HEAD
+<<<<<<< HEAD
 print('Hello, world!')
 print('Hello, world!')
 print('Hello, world!')
 
 
 
+=======
+print('33Hello, world!')
+>>>>>>> 783a88c... new cp
 =======
 print('33Hello, world!')
 >>>>>>> 783a88c... new cp
